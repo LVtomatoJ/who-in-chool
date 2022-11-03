4b4c1807ed57ca2ed85585c394c55d3eccdc0868
LVtomatoJ <992203755@qq.com>
2022-11-03 23:59:39 +0800

    add get_bind_info

--- shell/main.py
+++ shell/main.py
@@ -4,8 +4,11 @@ from bullet import styles
 import requests
 
 
+
 user = {}
 user_id = 0
+email = ""
+password = ""
 
 def print_user_info():
     global user
@@ -13,12 +16,33 @@ def print_user_info():
     for i in user.keys():
         print('*'+str(i)+" : "+str(user[i]))
 
+def print_bind_info():
+    bind_data = get_bind_info()
+    if bind_data['code']!=0:
+        return bind_data
+    data:list = bind_data['data']['bind_users']
+    for i in data:
+        print("=========")
+        for j in i.keys():
+            print('*'+str(j)+" : "+str(i[j]))
+    return bind_data
+
+def get_bind_info():
+    global password
+    global user_id
+    url = 'http://127.0.0.1:8000'
+    r = requests.get(url=url+"/v1/get_binds",params={'user_id':user_id,'password':password})
+    data = r.json()
+    return data
+    
 
 def login():
     cli = VerticalPrompt([Input('输入邮箱 : '),
                       Input('输入密码 : '),])
     result = cli.launch()
+    global email
     email = result[0][1]
+    global password
     password = result[1][1]
     url = 'http://127.0.0.1:8000'
     r = requests.get(url=url+"/v1/get_user_by_email",params={'email':email,'password':password})
@@ -40,6 +64,11 @@ def menu():
         print_user_info()
         return 1
     elif result=="查看绑定信息":
+        print_bind_info()
+        return 1
+    elif result=="查看任务信息":
+        return 0
+    elif result=="退出程序":
         return 0
     return 0
 
