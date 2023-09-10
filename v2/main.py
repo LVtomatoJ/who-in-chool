5ff9ed60f54b297e684544d04a92b6820c916f2f
LVtomatoj <992203755@qq.com>
2023-09-10 22:31:46 +0800

    bug

--- v2/main.py
+++ v2/main.py
@@ -336,7 +336,7 @@ async def customsign(bindid:str,city:str,longitude:str,country:str,district:str,
     return {'code':0,'msg':"自定义签到成功"}   
 #最新的自定义签到
 @app.get('/2/cutomsign_v2')
-async def customsign(bindid:str,city:str,longitude:str,country:str,district:str,township:str,latitude:str,province:str,nationcode:str,adccode:str,streetcode:str,citycode:str,towncode:str,street:str,auth = Depends(get_current_user_by_email)):
+async def customsign_v2(bindid:str,city:str,longitude:str,country:str,district:str,township:str,latitude:str,province:str,nationcode:str,adccode:str,streetcode:str,citycode:str,towncode:str,street:str,auth = Depends(get_current_user_by_email)):
     email = auth['email']
     res = await tools.custom_sign_v2(email=email,bindid=bindid,city=city,longitude=longitude,country=country,district=district,township=township,latitude=latitude,province=province,streetcode=streetcode,adccode=adccode,nationcode=nationcode,citycode=citycode,towncode=towncode,street=street)
     if res['code']!=0:
